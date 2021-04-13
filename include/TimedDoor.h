ad6d44338adab7e3caaea5a90b169fe1f6b11d9c
NoName <ihackit595@gmail.com>
2021-04-13 19:53:29 +0300

    finish functions

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -1,4 +1,4 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2021 Bogomazov
 
 #ifndef INCLUDE_TIMEDDOOR_H_
 #define INCLUDE_TIMEDDOOR_H_
@@ -24,7 +24,7 @@ class DoorTimerAdapter : public TimerClient {
  private:
   TimedDoor& door;
  public:
-  explicit DoorTimerAdapter(TimedDoor&);
+  explicit DoorTimerAdapter(TimedDoor& _door): door(_door) {}
   void Timeout();
 };
 
@@ -34,19 +34,20 @@ class TimedDoor : public Door {
   int iTimeout;
   bool opened;
  public:
-  explicit TimedDoor(int);
+  explicit TimedDoor(int timeout);
   bool isDoorOpened();
-  void unlock();
-  void lock();
+  void unlock() override;
+  void lock() override;
   void DoorTimeOut();
   void throwState();
+  int getTime();
 };
 
 class Timer {
   TimerClient *client;
-  void sleep(int);
+  void sleep(int ms);
  public:
-  void tregister(int, TimerClient*);
+  void tregister(int delay, TimerClient* tmcl);
 };
 
-#endif  // INCLUDE_TIMEDDOOR_H_
+#endif  //  INCLUDE_TIMEDDOOR_H_
--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,17 +1,20 @@
 // Copyright 2021 Bogomazov
 #include "../include/TimedDoor.h"
+#include <ctime>
+#include <string>
 
-bool TimedDoor::isDoorOpened() {
-    return opened;
+TimedDoor::TimedDoor(int timeout) {
+    iTimeout = timeout;
+    opened = false;
+    adapter = new DoorTimerAdapter(*this);
 }
 
-int TimedDoor::getITimeout() {
+int TimedDoor::getTime() {
     return iTimeout;
 }
 
-void DoorTimerAdapter::Timeout() {
-    Timer t = Timer();
-    t.tregister(door.getITimeout(), this);
+bool TimedDoor::isDoorOpened() {
+    return opened;
 }
 
 void TimedDoor::unlock() {
@@ -19,6 +22,34 @@ void TimedDoor::unlock() {
     opened = true;
 }
 
-TimedDoor::TimedDoor(int time) {
-    iTimeout = time;
+void TimedDoor::lock() {
+    opened = false;
+}
+
+void TimedDoor::DoorTimeOut() {
+    throw std::string("close the door!");
+}
+
+void TimedDoor::throwState() {
+  if (opened) {
+    throw std::string("the door is opened!");
+  } else {
+    throw std::string("the door is closed!");
+  }
+}
+
+void DoorTimerAdapter::Timeout() {
+    Timer t;
+    t.tregister(door.getTime(), this);
+    door.DoorTimeOut();
+}
+
+void Timer::sleep(int delay) {
+    time_t startTime = time(NULL);
+    while (time(NULL) - startTime < delay)
+        continue;
+}
+
+void Timer::tregister(int delay, TimerClient* tmcl) {
+    sleep(delay);
 }
